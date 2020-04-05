4ff806f7f59d02e134d03a75a4dbea56271b0862
Raphael Fontoura <raphael.fontoura@gmail.com>
2020-04-04 22:26:07 -0300

    Criado classes services implementando as interfaces services

---
+++ src/main/java/com/challenge/service/UserService.java
@@ -0,0 +1,35 @@
+package com.challenge.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.challenge.entity.User;
+import com.challenge.service.interfaces.UserServiceInterface;
+
+public class UserService implements UserServiceInterface {
+
+	@Override
+	public User save(User object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> findById(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findByAccelerationName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findByCompanyId(Long companyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
